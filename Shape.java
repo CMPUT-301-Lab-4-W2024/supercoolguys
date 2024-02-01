7a9fce6656c16ccd5b6ab6cc98556b52ade1cf05
jade 3 <jaderpana@Jades-MacBook-Air.local>
2024-02-01 13:16:41 -0700

    Shape colour string edited on Jade's branch

--- Shape.java
+++ Shape.java
@@ -2,6 +2,7 @@ abstract class Shape {
     //made the ints private to allow for proper encapuslation
     private int x;
     private int y;
+    String colour = "pink";
 
     public Shape(int x, int y){
         this.x = x;
