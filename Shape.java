9b3c1796f25f0520db8a6a8ee057d0572ab3a727
Moyo Dawodu <moyodawodu@gmail.com>
2024-02-01 15:27:51 -0700

    minor edit to spelling

--- Shape.java
+++ Shape.java
@@ -2,7 +2,7 @@ abstract class Shape {
     //made the ints private to allow for proper encapuslation
     private int x;
     private int y;
-    String colour = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y){
         this.x = x;
