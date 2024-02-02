5023cd34503b806a7c227c7bef3dcefc04e966d0
danylo nagornyi <danilnagornyi2015@gmail.com>
2024-02-02 15:32:36 -0700

    last part resolve

--- Shape.java
+++ Shape.java
@@ -2,7 +2,7 @@ abstract class Shape {
     //made the ints private to allow for proper encapuslation
     private int x;
     private int y;
-
+    //color
     String colour = "blue";
 
     public Shape(int x, int y) {
