30b26aa2e5ac578eb584f96c06d4e69116aa142f
danylo nagornyi <danilnagornyi2015@gmail.com>
2024-02-02 14:43:16 -0700

    fix

--- Shape.java
+++ Shape.java
@@ -2,18 +2,12 @@ abstract class Shape {
     //made the ints private to allow for proper encapuslation
     private int x;
     private int y;
-    String colour = "red";
 
-    public Shape(int x, int y){
+    String colour = "blue";
+
+    public Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX() {
-        return this.x;
-    }
-
-    public int getY(){
-        return this.y;
-    }
 }
